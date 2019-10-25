53733463b69e710d01668a3578814b374f69b168
Rudmanmrrod <rudmanmrrod@gmail.com>
2019-10-25 13:04:03 -0400

    Added initial structure for project

---
+++ src/config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDBdmtLS7EQqAET1HvGvym5Ef-yhTI8BWc",
+    authDomain: "react-blog-ecca4.firebaseapp.com",
+    databaseURL: "https://react-blog-ecca4.firebaseio.com",
+    projectId: "react-blog-ecca4",
+    storageBucket: "react-blog-ecca4.appspot.com",
+    messagingSenderId: "867290702909",
+    appId: "1:867290702909:web:3de66b6094e47a4cea3654",
+    measurementId: "G-YS845GYFZB"
+};
+
+export default firebaseConfig;
